c6c542a95e3e391128e2e8ae7c2dcdbe174976fb
Sadia Tabassum <tabassum1234@gmail.com>
2022-02-05 11:23:37 +0000

    Update CpjitsdpOPFilter.java

--- src/moa/tasks/CpjitsdpOPFilter.java
+++ src/moa/tasks/CpjitsdpOPFilter.java
@@ -1,5 +1,5 @@
 /*
- *    EvaluatePrequentialOfflineCPFilter.java
+ *    CpjitsdpOPFilter.java
  *    
  *    Copyright (C) 2022 University of Birmingham, Birmingham, United Kingdom
  *    @author Sadia Tabassum (sxt901@student.bham.ac.uk)
